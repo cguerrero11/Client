4d2da8b1d0c2f73b0cb008b363d08b7a3f8be2e7
cguerrero1 <cguerrero1@my.wctc.edu>
2019-09-10 13:43:37 -0500

    Work done in class

---
+++ Client/wwwroot/js/client.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+$(function () {
+    $('.code').on('click', function (e) {
+        // preload audio
+        var toast = new Audio('media/toast.wav');
+        e.preventDefault();
+        // first pause the audio (in case it is still playing)
+        toast.pause();
+        // reset the audio
+        toast.currentTime = 0;
+        // play audio
+        toast.play();
+        $('#toast').toast({ autohide: false }).toast('show');
+    });
+});
